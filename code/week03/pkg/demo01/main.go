f31f29c7f4f1a8c3f58c1f50f9e5e1c47990c752
victorfengming <54264304+victorfengming@users.noreply.github.com>
2021-10-25 15:01:05 +0800

    main 自己做也可以

--- code/week03/pkg/demo01/main.go
+++ code/week03/pkg/demo01/main.go
@@ -7,17 +7,13 @@ import (
 )
 
 func main() {
-	fmt.Println("rsat")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "hello, gopherCon SG")
 	})
 
-	go func() {
-		if err := http.ListenAndServe(":8080", nil); err != nil {
-			log.Fatal(err)
-		}
-	}()
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 
-	// 为了让他不死,就要写个死循环
-	select {}
 }
