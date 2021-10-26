a2184b13a1f0c15cfd7b1298b9d9f7f439d60cdd
victorfengming <54264304+victorfengming@users.noreply.github.com>
2021-10-26 15:25:08 +0800

    web ok

---
+++ mooc/code/learngo/errhandling/file_listing_server/web.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/list/", func(writer http.ResponseWriter, request *http.Request) {
+		//Path截取 切片 [ i:]
+		path := request.URL.Path[len("/list/"):]
+		file, err := os.Open(path)
+		if err != nil {
+			// 处理
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+		all, err := ioutil.ReadAll(file)
+		if err != nil {
+			panic(err)
+		}
+		writer.Write(all)
+	})
+	err := http.ListenAndServe(":8888", nil)
+	if err != nil {
+		panic(err)
+	}
+}
