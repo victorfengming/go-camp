1ef7124590aa766837baf8dd83b9e395591b4a3e
victorfengming <54264304+victorfengming@users.noreply.github.com>
2021-10-26 16:46:52 +0800

    处理两个模块之间的错误 包装

--- mooc/code/learngo/errhandling/file_listing_server/web.go
+++ mooc/code/learngo/errhandling/file_listing_server/web.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	//"io/ioutil"
+	log "github.com/sirupsen/logrus"
 	"learngo/errhandling/file_listing_server/file_listing"
+	//"io/ioutil"
 	"net/http"
 	"os"
 	//"os"
 )
 
 func main() {
-	http.HandleFunc("/list/", errWrapper(file_listing.HandleFileList))
+	http.HandleFunc("/", errWrapper(file_listing.HandleFileList))
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		panic(err)
@@ -20,13 +21,22 @@ type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
 func errWrapper(handler appHandler) func(writer http.ResponseWriter, request *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			r := recover()
+			log.Printf("Painic: %v", r)
+			http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}()
 		err := handler(writer, request)
 		if err != nil {
+			log.Warn("Error handling request:%s"+
+				"", err.Error())
 			code := http.StatusOK
 			// 错误处理
 			switch {
 			case os.IsNotExist(err):
 				code = http.StatusNotFound
+			case os.IsPermission(err):
+				code = http.StatusForbidden
 			default:
 				code = http.StatusInternalServerError
 			}
