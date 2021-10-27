f457ec6dc3442ec46730ae3f0b43fb5e83793c77
victorfengming <54264304+victorfengming@users.noreply.github.com>
2021-10-27 10:40:51 +0800

    go 放到里面 来做

--- mooc/code/learngo/channel/channel.go
+++ mooc/code/learngo/channel/channel.go
@@ -5,10 +5,14 @@ import (
 	"time"
 )
 
-func worker(id int, c chan int) {
-	for {
-		fmt.Printf("%d,%c\n", id, <-c)
-	}
+func worker(id int) chan int {
+	c := make(chan int)
+	go func() {
+		for {
+			fmt.Printf("@%d---%c\n", id, <-c)
+		}
+	}()
+	return c
 }
 
 func chanDemo() {
@@ -16,8 +20,7 @@ func chanDemo() {
 	var cahnneles [10]chan int
 	for i := 0; i < 10; i++ {
 		//var c chan int // c == nil
-		cahnneles[i] = make(chan int)
-		go worker(i, cahnneles[i])
+		cahnneles[i] = worker(i)
 	}
 
 	for i := 0; i < 10; i++ {
@@ -35,28 +38,28 @@ func main() {
 }
 
 /**
-5,f
-2,c
-0,a
-1,b
-3,d
-0,A
-1,B
-9,j
-8,i
-4,e
-4,E
-3,D
-7,h
-2,C
-6,g
-6,G
-7,H
-9,J
-5,F
-8,I
+
+@0---a
+@8---i
+@1---b
+@1---B
+@3---d
+@4---e
+@5---f
+@6---g
+@7---h
+@0---A
+@9---j
+@2---c
+@2---C
+@5---F
+@6---G
+@7---H
+@3---D
+@4---E
+@9---J
+@8---I
 
 Process finished with the exit code 0
-
 
 */
