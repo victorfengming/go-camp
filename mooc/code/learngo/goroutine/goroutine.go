4ce0b7b5b6db9a1c6242d78445ffb8e0763e9d19
victorfengming <54264304+victorfengming@users.noreply.github.com>
2021-10-27 09:57:28 +0800

    add img for goroutine vs other language

--- mooc/code/learngo/goroutine/goroutine.go
+++ mooc/code/learngo/goroutine/goroutine.go
@@ -1,15 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
 
 func main() {
 	//fmt.Printf("hellow from goroutine %d")
+	var a [10]int
 	for i := 0; i < 10; i++ {
-		go func(ii int) {
+		go func() {
+			// race condition
 			for {
-				fmt.Printf("hellow from goroutine %d\n", ii)
+				a[i]++
+				//fmt.Printf("hellow from goroutine %d\n", ii)
+				// 交出控制权
+				runtime.Gosched()
 			}
-		}(i)
+		}()
 	}
-	select {}
+	time.Sleep(time.Millisecond)
+	fmt.Println(a)
 }
+
+/**
+PS E:\Projects\GolandProjects\go-camp\mooc\code\learngo\goroutine> go run .\goroutine.go -race
+panic: runtime error: index out of range [10] with length 10
+
+goroutine 15 [running]:
+main.main.func1()
+        E:/Projects/GolandProjects/go-camp/mooc/code/learngo/goroutine/goroutine.go:16 +0x56
+created by main.main
+        E:/Projects/GolandProjects/go-camp/mooc/code/learngo/goroutine/goroutine.go:13 +0x57
+panic: runtime error: index out of range [10] with length 10
+
+goroutine 10 [running]:
+main.main.func1()
+        E:/Projects/GolandProjects/go-camp/mooc/code/learngo/goroutine/goroutine.go:16 +0x56
+created by main.main
+        E:/Projects/GolandProjects/go-camp/mooc/code/learngo/goroutine/goroutine.go:13 +0x57
+exit status 2
+PS E:\Projects\GolandProjects\go-camp\mooc\code\learngo\goroutine>
+
+*/
