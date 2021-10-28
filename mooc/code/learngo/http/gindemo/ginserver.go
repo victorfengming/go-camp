b5b7d1952b2b07c34ec2796214814484d417f0a6
victorfengming <54264304+victorfengming@users.noreply.github.com>
2021-10-28 14:54:17 +0800

    中间件 拦截

--- mooc/code/learngo/http/gindemo/ginserver.go
+++ mooc/code/learngo/http/gindemo/ginserver.go
@@ -1,13 +1,39 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"time"
+)
 
 func main() {
 	r := gin.Default()
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	// middleware
+	r.Use(func(c *gin.Context) {
+		s := time.Now()
+
+		c.Next()
+
+		// 不管访问什么,都能先进到这里面来
+		// log latency, response code
+		logger.Info("incoming request:",
+			zap.String("path", c.Request.URL.Path),
+			zap.Int("status", c.Writer.Status()),
+			zap.Duration("elapsed", time.Now().Sub(s)),
+		)
+		//log.Fatalf(c.Request.URL.Path)
+	})
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(200, "hello gin")
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
