2ca023b5826be8cf5097308c4bb420afd09eb6e5
victorfengming <54264304+victorfengming@users.noreply.github.com>
2021-10-28 17:32:19 +0800

    go home

--- mooc/code/coolcar/server/hello.go
+++ mooc/code/coolcar/server/hello.go
@@ -1,14 +1,58 @@
 package main
 
 import (
+	trippb "coolcar/proto/gen/go"
+	"encoding/json"
 	"fmt"
 
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
-	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+	"github.com/golang/protobuf/proto"
 )
 
 func main() {
-	fmt.Println("hello")
+	trip := trippb.Trip{
+		Start:       "abc",
+		End:         "def",
+		DurationSec: 36000,
+		FeeCent:     13000,
+		StartPos: &trippb.Location{
+			Latitude:30,
+			Longitude: 120,
+		},
+		EndPos: &trippb.Location{
+			Latitude:30,
+			Longitude: 120,
+		},
+		PathLocaitons: []*trippb.Location{
+			{
+				Latitude:31,
+				Longitude: 118,
+			},
+			{
+				Latitude:37,
+				Longitude: 125,
+			},
+		},
+	}
+
+	fmt.Println(&trip)
+	b, err := proto.Marshal(&trip)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%X\n", b)
+	var trip2 trippb.Trip
+	proto.Unmarshal(b,&trip2)
+	
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(&trip2)
+
+	bb,err:=json.Marshal(&trip2)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("json:%s\n",bb)
 }
