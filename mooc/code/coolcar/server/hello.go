f83a3b57c4bcb0d0292881c85be5db5ff1108197
victorfengming <54264304+victorfengming@users.noreply.github.com>
2021-10-28 16:27:32 +0800

    add trip

--- mooc/code/coolcar/server/hello.go
+++ mooc/code/coolcar/server/hello.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
 
 func main() {
 	fmt.Println("hello")
